1f6191ec5609db868af6f15a798b1f6252818c4d
amirhosein <ahosein54@gmail.com>
2025-03-17 13:27:11 +0000

    added some conf files and proccessing completion api endpoint on nginx

---
+++ system_prompts/retrieve_agent.py
@@ -0,0 +1,4 @@
+system_prompt = """
+You are an intelligent assistant that retrieves relevant information from a knowledge base. 
+Provide concise and accurate responses based on the user's query and context.
+"""
