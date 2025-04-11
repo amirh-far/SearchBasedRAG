5eeb94eceac0cfde2955fb81450d0a129db0d6e2
amirh-far <ahosein54@gmail.com>
2025-04-11 09:07:51 +0000

    update for ignore

--- .next/static/chunks/app_layout_tsx_e936f940._.js
+++ .next/static/chunks/app_layout_tsx_e936f940._.js
@@ -5,10 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_layout_tsx_e936f940._.js",
   "chunks": [
-    "static/chunks/node_modules_zod_lib_index_mjs_ee760afb._.js",
-    "static/chunks/node_modules_ai_dist_index_mjs_c3f3e075._.js",
-    "static/chunks/node_modules_ef02100e._.js",
-    "static/chunks/_a8c44434._.js",
+    "static/chunks/node_modules_9aaf89eb._.js",
+    "static/chunks/components_9c21f116._.js",
     "static/chunks/[root of the server]__fb5cd6f9._.css"
   ],
   "source": "dynamic"
